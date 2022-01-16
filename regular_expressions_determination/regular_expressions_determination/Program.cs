3793646d4370ea49f2be06ae839a806a1cd54f21
Даниил Герасимов <daniil.gerasimov@travelline.ru>
2022-01-16 20:44:51 +0300

    Finsih epsilon closures

---
+++ regular_expressions_determination/regular_expressions_determination/Program.cs
@@ -0,0 +1,86 @@
+﻿using determination;
+
+const char _delimeter = ' ';
+const char _descriptionsDelimeter = '|';
+const string Epsilon = "e";
+
+Console.Write( "Type input file name: " );
+string sourceFileName = Console.ReadLine();
+Console.WriteLine();
+
+Console.Write( "Type output file name: " );
+string outputFileName = Console.ReadLine();
+Console.WriteLine();
+
+string path = Path.Combine( Environment.CurrentDirectory, $"{sourceFileName}.txt" );
+StreamReader reader = new( path );
+
+string outputFilePath = Path.Combine( Directory.GetParent( Directory.GetCurrentDirectory() ).Parent.Parent.Parent.FullName, $"{outputFileName}.txt" );
+StreamWriter _output = new( File.Open( outputFilePath, FileMode.Create ) )
+{
+    AutoFlush = true
+};
+
+Dictionary<string, List<TransitionInfo>> transitionInfosByState = new()
+{
+};
+
+string[] grammarDescriptions = reader.ReadToEnd().Split( Environment.NewLine );
+foreach ( string grammarDescription in grammarDescriptions )
+{
+    string[] descriptionTokens = grammarDescription.Split( _delimeter );
+    string unit = descriptionTokens[ 0 ][ 0..^1 ].ToString();
+    transitionInfosByState.Add( unit, new() );
+
+    string[] transitions = descriptionTokens[ 1 ].Split( _descriptionsDelimeter );
+    foreach ( string transition in transitions )
+    {
+        TransitionInfo transitionInfo = TransitionInfo.FromSymbolAndTransition( unit, transition );
+        transitionInfosByState[ unit ].Add( transitionInfo );
+    }
+}
+
+transitionInfosByState.Add( TransitionInfo._finalState, new List<TransitionInfo>() );
+
+Dictionary<string, List<string>> achievableWithEpsilonByState = new();
+Dictionary<string, List<TransitionInfo>> newTransitionsByState = new();
+
+foreach ( (string stateName, List<TransitionInfo> transitions) in transitionInfosByState )
+{
+    achievableWithEpsilonByState[ stateName ] = new() { stateName };
+    List<string> achievableByEpsilon = transitions.Where( ti => ti.Signal == Epsilon ).Select( ti => ti.To ).Distinct().ToList();
+    achievableWithEpsilonByState[ stateName ].AddRange( achievableByEpsilon );
+
+    List<string> tempAchievables = achievableByEpsilon.ToList();
+    while ( tempAchievables.Any() )
+    {
+        List<string> newAchievables = new();
+        foreach ( string tempAchievable in tempAchievables )
+        {
+            newAchievables.AddRange( transitionInfosByState[ tempAchievable ].Where<TransitionInfo>( ti => ti.Signal == Epsilon ).Select<TransitionInfo, string>( ti => ti.To ).Distinct<string>() );
+        }
+
+        tempAchievables = newAchievables.Distinct().ToList();
+        achievableWithEpsilonByState[ stateName ].AddRange( tempAchievables );
+    }
+
+
+    HashSet<TransitionInfo> newTransitions = new();
+    foreach ( string achievableStateName in achievableWithEpsilonByState[ stateName ] )
+    {
+        List<TransitionInfo> transitionsOfAchievableState = transitionInfosByState[ achievableStateName ].Where( ti => ti.Signal != Epsilon ).ToList();
+        IEnumerable<TransitionInfo> b = transitionsOfAchievableState.Select( ti => new TransitionInfo { From = stateName, Signal = ti.Signal, To = ti.To } );
+        newTransitions.UnionWith( b );
+    }
+
+    newTransitionsByState[ stateName ] = newTransitions.ToList();
+}
+
+foreach ( (string state, List<TransitionInfo> transitionInfos) in newTransitionsByState )
+{
+    if ( state != TransitionInfo._finalState )
+    {
+        string b = string.Join( "|", transitionInfos );
+        await _output.WriteLineAsync( $"{state}: {b}" );
+    }
+}
---
+++ regular_expressions_determination/regular_expressions_determination/TransitionInfo.cs
@@ -0,0 +1,55 @@
+﻿namespace determination
+{
+    public class TransitionInfo : IEquatable<TransitionInfo>
+    {
+        public const string _finalState = "F";
+
+        public string From { get; set; }
+        public string Signal { get; set; }
+        public string To { get; set; }
+
+        public static TransitionInfo FromSymbolAndTransition( string symbol, string transition )
+        {
+            if ( transition.Length == 1 )
+            {
+                return new TransitionInfo
+                {
+                    From = symbol,
+                    Signal = transition,
+                    To = _finalState
+                };
+            }
+
+            return new TransitionInfo
+            {
+                From = symbol,
+                Signal = transition[ 0 ].ToString(),
+                To = transition[ 1 ].ToString()
+            };
+        }
+
+        public override string ToString()
+        {
+            string nonFinalTo = To == _finalState ? null : To;
+            return $"{Signal}{nonFinalTo}";
+        }
+
+        public bool Equals( TransitionInfo other )
+        {
+            return other is TransitionInfo connection
+                     && From == connection.From
+                     && Signal == connection.Signal
+                     && To == connection.To;
+        }
+
+        public override bool Equals( object obj )
+        {
+            return Equals( obj as TransitionInfo );
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine( From, Signal, To );
+        }
+    }
+}
