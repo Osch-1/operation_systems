a5e1abdb207e0488c59fa7220c98974d0d66fdcc
Даниил Герасимов <daniil.gerasimov@travelline.ru>
2022-01-26 09:45:14 +0300

    Fix input handling

--- ll_parser/ll_parser/Program.cs
+++ ll_parser/ll_parser/Program.cs
@@ -12,24 +12,11 @@ FileStream inputFileStream = File.OpenRead( inputFilePath );
 ILLRulesReader reader = new LLRulesReader( inputFileStream );
 LLParser.LLParser parser = new( reader );
 
-/*string? line = "";
+string? line = "";
 while ( ( line = Console.ReadLine() ) != null )
 {
     bool isMatch = parser.IsMatch( line );
     string noun = isMatch ? "is" : "isn't";
 
     Console.WriteLine( $"{line} {noun} matches the rules" );
-}*/
-
-List<string> tests = new List<string>()
-            {
-                "-a", "-7", "-a*", "-()", "*()", "(*)", "(-)", "(a)", "(7)", "-77)7", "a-77a7a7*-77a",
-                "(-a7", "(7+7)", "((7+7))", "(((7+7)))",
-                "(((7+7)))+7", "(((7+7))+7)", "(((7+7)+7))",
-                "(7*7)", "((7*7))", "(((7*7)))", "(((7*7)))*7", "(((7*7))*7)", "(((7*7)*7))"
-            };
-
-foreach ( var item in tests )
-{
-    Console.WriteLine( parser.IsMatch( item ) );
 }
